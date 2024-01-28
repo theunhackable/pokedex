f9933d442a9d4e53373c60cf1b3d0d44fed28688
theunhackable <srirangasaipagala@gmail.com>
2024-01-28 15:39:58 +0530

    added basic NavBar

--- components/menus/NavBar.tsx
+++ components/menus/NavBar.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from "react";
+import ThemeSwitcher from "../ThemeSwitcher";
+import SearchBar from "../SearchBar";
+import PokeBallLogo from "../icons/PokeBallLogo";
+import Link from "next/link";
 
 export const NavBar = () => {
-  
   return (
-    <div>NavBar</div>
-  )
-}
+    <header className="sticky top-0 p-5 bg-primary-red text-white z-50">
+      <nav className="flex items-center justify-between">
+        <Link href='/' className="flex items-center gap-1">
+          <PokeBallLogo className="h-7 w-7" />
+          <h1 className="text-2xl font-bold">
+            Pokédex
+          </h1>
+        </Link>
+        <div className="flex items-center gap-5">
+        <div>
+          <Link href='/pokemons'>Pokemons </Link>
+        </div>
+        <SearchBar />
+        <ThemeSwitcher />
+        </div>
+      </nav>
+    </header>
+  );
+};
