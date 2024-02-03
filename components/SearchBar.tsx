7d7fc60931c2b319a4fd938a90be85cc9b6f2760
theunhackable <srirangasaipagala@gmail.com>
2024-02-03 21:55:18 +0530

    added persistent theme through localstorage

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,7 +3,7 @@ import React, { Dispatch, SetStateAction, useState } from 'react'
 import {Input} from '@/components/ui/input'
 const SearchBar = ({setSearch}:{setSearch:  Dispatch<SetStateAction<string>>}) => {
   return (
-    <Input  onChange={(e) => setSearch(() => e.target.value)} className='max-w-80 max-h-8' placeholder='Search'/>
+    <Input name='search-bar'  onChange={(e) => setSearch(() => e.target.value)} className='max-w-80 max-h-8' placeholder='Search'/>
     )
 }
 
--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -7,16 +7,25 @@ const ThemeSwitcher = () => {
   const { setTheme } = useTheme()
   const [isChecked, setIsChecked] = useState(false)
   useEffect(() => {
+    const userTheme = localStorage.getItem('theme')
+    if(userTheme && userTheme === 'dark') {
+      setIsChecked(true)
+    }
+  }, [])
+  useEffect(() => {
     if(isChecked === false) {
       setTheme('light')
+      localStorage.setItem('theme', 'light')
     } else {
       setTheme('dark')
+      localStorage.setItem('theme', 'dark')
+
     }
   }, [isChecked])
   return (
     <div className='flex items-center gap-2'>
       <SunIcon className=''/>
-      <Switch id='theme-switcher' className='data-[state=unchecked]:bg-red-300 dark:bg-red-300' checked={isChecked} onCheckedChange={setIsChecked}/>
+      <Switch name='theme-switcher' id='theme-switcher' className='data-[state=unchecked]:bg-red-300 dark:bg-red-300' checked={isChecked} onCheckedChange={setIsChecked}/>
       <MoonIcon /> 
     </div>
   )
